6cf0912b584a7966cf0f44c285482aa7d5241521
Daniel_S <69630750+konannik@users.noreply.github.com>
2022-10-30 12:08:29 +0100

    Basic, Swagger & Connection to MySqlDb

---
+++ src/main/java/pl/agh/edu/recomendationsystem/RecomendationSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.recomendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/agh/edu/recomendationsystem/RecomendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.recomendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
