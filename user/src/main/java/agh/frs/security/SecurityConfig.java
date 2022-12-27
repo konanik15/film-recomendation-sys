a72ec7e4582248056c3f807c8398148aa6d188c6
gploc <gploc@DESKTOP-6LUVO7C>
2022-12-27 22:18:25 +0100

    Cors changes

--- user/src/main/java/agh/frs/security/SecurityConfig.java
+++ user/src/main/java/agh/frs/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
+                .cors().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(authEntryPoint)
                 .and()
@@ -42,7 +48,7 @@ public class SecurityConfig {
 //                .antMatchers("/api/user/getUserList").hasRole("USER")
                 .antMatchers("/swagger-ui/**", "/v3/api/docks/**").permitAll()
                 .antMatchers("/api/user/**").permitAll()
-                .antMatchers(("/api/movie/**")).permitAll()
+                .antMatchers(("/api/movie/**")).authenticated()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
@@ -53,6 +59,21 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
+                "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type",
+                "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
--- user/src/main/java/agh/frs/security/SecurityConstant.java
+++ user/src/main/java/agh/frs/security/SecurityConstant.java
@@ -1,6 +1,6 @@
 package agh.frs.security;
 
 public class SecurityConstant {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 70000000;
     public static final String JWT_SECRET = "secret";
 }
